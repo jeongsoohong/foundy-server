e644e8eee0122b351c78a57c11ca037509c1a51e
jaehyuk.hyun <jaehyuk.hyun@atto-research.com>
2020-08-10 11:08:04 +0900

    sweettracker api key 변경

--- application/views/back/shop/shop_shipping.php
+++ application/views/back/shop/shop_shipping.php
@@ -123,7 +123,7 @@
   var shipping_company_codes = [<?php foreach ($shop_shipping_company as $c) { echo "'".$c->shipping_company_code."',";} ?>];
   function get_shipping_company() {
     $.ajax({
-      url: 'https://info.sweettracker.co.kr/api/v1/companylist?t_key=tKYfi3VIoSFbLfNO83PxYQ',
+      url: 'https://info.sweettracker.co.kr/api/v1/companylist?t_key=OiMqBmE0a9g8ibCUoY8yng',
       type: 'GET', // form submit method get/post
       cache: false,
       contentType: 'application/json',
