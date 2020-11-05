6f9439cd092c0156ae7c6331c1fa15bd8827fad8
jaehyuk.hyun <jaehyuk.hyun@atto-research.com>
2020-11-05 14:53:48 +0900

    푸터 정보 변경

--- application/views/front/footer/footer.php
+++ application/views/front/footer/footer.php
@@ -4,10 +4,10 @@
       <div class="row">
         <div class="col-md-12 col-xs-12">
           <div class="copyright" version="Currently v0.1">
-            파이브앤다임 | 대표 : 홍정수 | 통신판매업신고번호 : 제 2020-서울서초-1222호 ㅣ 사업자등록번호 : 454-16-00611(
+            주식회사 파운디 | 대표 : 신두철 | 통신판매업신고번호 : 제 2020-서울용산-1881호 ㅣ 사업자등록번호 : 658-87-01852(
             <a href="javascript:void(0);" onclick="onopen('business');"><u>사업자정보확인</u></a>
             )<br>
-            주소 : 서울특별시 서초구 강남대로 527, 브랜드칸타워 11층(반포동) ㅣ 전화 : <a href="tel:070-8656-8895">070-8656-8895</a><br>
+            주소 : 서울특별시 마포구 성미산로 19길 21 202호 ㅣ 전화 : <a href="tel:070-8656-8895">070-8656-8895</a><br>
             이메일 : <a href="mailto:hello@foundy.me">hello@foundy.me</a><br>
             개인정보보호책임자 홍정수(<a href="mailto:hello@foundy.me">hello@foundy.me</a>) |
             <a href="javascript:void(0);" onclick="onopen('service')"><u>서비스이용약관</u></a> |
@@ -39,7 +39,7 @@
   function onopen(input) {
     var url;
     if (input === 'business') {
-      url = "http://www.ftc.go.kr/bizCommPop.do?wrkr_no=4541600611"
+      url = "http://www.ftc.go.kr/bizCommPop.do?wrkr_no=6588701852"
       window.open(url, "bizCommPop", "width=750, height=700;");
     } else if (input === 'service') {
       url = "<?php echo base_url().'home/user/service'; ?>";
