c985a5c7ec6469b0939bb5ebbbeee80e2f6862d3
jaehyuk.hyun <jaehyuk.hyun@atto-research.com>
2020-11-05 23:28:50 +0900

    크톤 컨트롤러 추가 / 테스트

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -22,7 +22,7 @@ class Cron extends CI_Controller
     echo $date;
     if ($para1 == 'delete') {
       $query = <<<QUERY
-select a.user_id from user a, user_unregister b where a.user_id=b.user_id and a.unregister=1 and b.unregister_at<'{$date}'
+select a.* from user a, user_unregister b where a.user_id=b.user_id and a.unregister=1 and b.unregister_at<'{$date}'
 QUERY;
       $result = $this->db->query($query)->result();
 
